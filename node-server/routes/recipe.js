c50a9419ae1ab32c14f0bd0f7f322979a1be7d83
Paul Breton <paul.ravezb@gmail.com>
2016-12-01 20:55:13 +0100

    Début de description d'un recipe

--- node-server/routes/recipe.js
+++ node-server/routes/recipe.js
@@ -4,10 +4,17 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    postgreaccess.doQuery(queries.recipes.everything, [], function(results){
-        console.log(results);
-        res.render('recipes', { rows: results, recipes: true });
-    });
+    if(req.query["id"]){
+        postgreaccess.doQuery(queries.recipes.query_id, [req.query["id"]], (results) => {
+            console.log(results);
+            res.render('recipe', {rows: results, recipes: false});
+        });
+    }else {
+        postgreaccess.doQuery(queries.recipes.everything, [], (results) => {
+            console.log(results);
+            res.render('recipes', {rows: results, recipes: true});
+        });
+    }
 });
 
 
