a1ead58952ffa8ea64768bd8cd9800a6827df331
Paul Breton <paul.ravezb@gmail.com>
2016-12-08 20:18:33 +0100

    Corrections sur les recettes. Il faudrait peut être voir pour ajouter la possibilité de chercher plusieurs catégories à la fois.

--- node-server/public/javascripts/search_recipes.js
+++ node-server/public/javascripts/search_recipes.js
@@ -1,14 +1,14 @@
 let selected_servings = 2;
 let selected_category = 0;
 
-$("#servings-list").find("li a").on("click", function(e){
+$("#servings-list").find("li a").on("click", function (e) {
     $("#servings-list").find("li").removeClass("active");
     $(this).parent().addClass("active");
     selected_servings = parseInt($(this).parent().attr("id-selection"));
     search_value();
 });
 
-$("#categories-list").find("li a").on('click', function(e){
+$("#categories-list").find("li a").on('click', function (e) {
     $("#categories-list").find("li").removeClass("active");
     $(this).parent().addClass("active");
     selected_category = parseInt($(this).parent().attr("id-category"));
@@ -16,11 +16,11 @@ $("#categories-list").find("li a").on('click', function(e){
     search_value();
 });
 
-function test_servings(type, value, totest){
-    if(value < 0)
+function test_servings(type, value, totest) {
+    if (value < 0)
         return true;
 
-    switch(type){
+    switch (type) {
         case 1:
             return value == totest;
         case 2:
@@ -32,34 +32,39 @@ function test_servings(type, value, totest){
     return true;
 }
 
-function search_value(){
+function search_value() {
     let search_name = $("#search_name").val();
     let search_servings = $("#search_servings").val();
     let search_category = $("#search_categories").val();
-    if(search_servings === "")
+    if (search_servings === "")
         search_servings = -1;
 
-    if(search_category === "" || search_category === "All")
+    if (search_category === "" || search_category === "All")
         search_category = "";
 
     let found = 0;
 
-    rows.forEach(function(e){
-        if(e[name].match(new RegExp(search_name, 'i')) && test_servings(selected_servings, parseInt(search_servings), e[quantity])
-        && e[category].match(new RegExp(search_category, "i"))){
-            document.querySelector('['+id_field+'="'+e[id]+'"]').style.display = "";
-            found ++;
-        }else{
-            document.querySelector('['+id_field+'="'+e[id]+'"]').style.display = "none";
+    rows.forEach(function (e) {
+        if (e[name].match(new RegExp(search_name, 'i')) && test_servings(selected_servings, parseInt(search_servings), e[quantity]) && validate_category(e, new RegExp(search_category, "i"))) {
+            document.querySelector('[' + id_field + '="' + e[id] + '"]').style.display = "";
+            found++;
+        } else {
+            document.querySelector('[' + id_field + '="' + e[id] + '"]').style.display = "none";
         }
     });
 
-    if(found > 0)
+    if (found > 0)
         document.querySelector("#nothing-found").style.display = "none";
     else
         document.querySelector("#nothing-found").style.display = "";
 }
 
-function validate_category(){
-    search_value();
+function validate_category(element, regex) {
+    for (var i =0;i<element[category].length;i++){
+        if (element[category][i].match(regex)) {
+            return true;
+        }
+    }
+
+    return false;
 }
--- node-server/routes/recipe.js
+++ node-server/routes/recipe.js
@@ -13,12 +13,36 @@ router.get('/', function(req, res, next) {
         postgreaccess.doQuery(queries.recipes.everything, [], function(results){
             postgreaccess.doQuery(queries.categories.all, [], function(categories){
                 console.log(categories);
-                //console.log(results);
-                res.render('recipes', {rows: results, categories: categories, recipes: true, session: req.session})
+                var computed = compute_categories(results);
+                res.render('recipes', {rows: computed, categories: categories, recipes: true, session: req.session})
             });
         });
     }
 });
+
+function  compute_categories(results){
+    var newResults = {};
+
+    results.forEach(function(e){
+         if(!newResults[e["id_recipe"]]){
+             newResults[e["id_recipe"]] = e;
+             var category = e["category_name"];
+             newResults[e["id_recipe"]]["category_name"] = [category];
+         }else{
+             newResults[e["id_recipe"]]["category_name"].push(e["category_name"]);
+         }
+    });
+
+    var final = [];
+
+    for(var e in newResults){
+        if(newResults.hasOwnProperty(e)){
+            final.push(newResults[e]);
+        }
+    }
+
+    return final;
+}
 
 
 module.exports = router;
