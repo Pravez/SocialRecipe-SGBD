4079aa3ce2c629aa5f9102c5d16d56e7846943bb
Paul Breton <paul.ravezb@gmail.com>
2016-12-10 13:06:31 +0100

    Ajout (rapide) de la description d'un ingrédient, à voir pour l'améliorer

--- node-server/routes/ingredient.js
+++ node-server/routes/ingredient.js
@@ -4,9 +4,9 @@ var router = express.Router();
 /* GET ingredients page. */
 router.get('/', function (req, res) {
     if (req.query["id"]) {
-        postgreaccess.doQuery(queries.ingredients.query_id, [req.query["id"]], function (results) {
+        postgreaccess.doQuery(queries.ingredients.query_id_nc, [req.query["id"]], function (results) {
             console.log(results);
-            res.render('ingredient', {rows: results, session: req.session});
+            res.render('ingredient', {rows: results, session: req.session, ingredients:true});
         });
     } else {
         postgreaccess.doQuery(queries.ingredients.all, [], function (results) {
